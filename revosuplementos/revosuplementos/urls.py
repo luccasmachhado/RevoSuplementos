91c880bca12d5f8eca9f7a5af79542594b506273
Lucas Machado <lucasmachado.2018@outlook.com>
2026-03-12 18:57:40 -0300

    ajustando imagens para produção

--- revosuplementos/revosuplementos/urls.py
+++ revosuplementos/revosuplementos/urls.py
@@ -26,3 +26,9 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+else:
+    # Em produção (Render), usamos esta rota para servir arquivos de mídia
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
